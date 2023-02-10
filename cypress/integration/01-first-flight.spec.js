892a99374495df066df3f51ebfea7940b3355a52
elearningplugins <support@elearningfreak.com>
2023-02-09 22:59:46 -0600

    Initial practice
    Intro and Testing Basics from https://frontendmasters.com/courses/cypress/aliases/

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,16 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+    beforeEach(() => {
+      cy.visit('/jetsetter'); 
+    });
+    it('should have a form', () => {
+        cy.get('form');
+    });
+    it('should have the words "Add Item"', () => {
+        cy.contains('Add Item');
+    });
+    it('should put stuff in the input field', () => {
+        cy.get('[data-test="new-item-input"]').type('Good Attitude');
+    });
+});
